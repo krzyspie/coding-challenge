6d76a3a0bab582c88c752088d88a2191e89b2751
krzpie <krzys_7@op.pl>
2025-10-01 20:20:08 +0200

    Add phone number formatter challenge projects

---
+++ tests/PhoneNumberFormatterTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PhoneNumberFormatterTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
